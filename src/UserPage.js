fad5a22573d67502e79e2e6466d7d22f9ac0dc6b
jzanderson09 <jzanderson09@gmail.com>
2019-07-22 20:26:58 -0600

    Still struggling with searching this.state for specific target/clicked item to change completed to true.  Also need to change class of bucketlist Item so styling can change from none to line-through and vice versa (toggle).

--- src/UserPage.js
+++ src/UserPage.js
@@ -1,22 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './UserPage.css';
 
-const UserPage = () => {
-    return (
-        <div className='UserPage-container'>
-            <h1 className='greeting'>Welcome, Jake!</h1>
-            <div className='bucketlist-container'>
-                <ul className='bucketlist'>
-                    <h3>Your bucket list:</h3>
-                    <li>Walk the dog</li>
-                    <li>Wash the dishes</li>
-                    <li>Mow the lawn</li>
-                    <li>Pay rent</li>
-                </ul>
+const myBucketList = [
+    {
+        id: 0,
+        task: 'Walk the dog',
+        completed: false
+    },
+    {
+        id: 1,
+        task: 'Wash the dishes',
+        completed: false
+    },
+    {
+        id: 2,
+        task: 'Mow the lawn',
+        completed: false
+    },
+];
+
+class UserPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            bucketItem: '',
+            bucketList: myBucketList
+        }
+    }
+
+    addBucketItem = event => {
+        event.preventDefault();
+        this.setState({ bucketList: [...this.state.bucketList, this.state.bucketItem] });
+        this.setState({ bucketItem: '' });
+    }
+
+    changeHandler = event => {
+        event.preventDefault();
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    complete = event => {
+        const target = event;
+        console.log(target);
+        // const targetFound = this.state.bucketList.find(obj => obj.id === target.id);
+        if (target.completed === false) {
+            this.setState({ [target.completed]: true });
+        }
+        else {
+            this.setState({ [target.completed]: false });
+        }
+    }
+
+    //myArray.find(x => x.id === '45').foo;
+
+    render() {
+        return (
+            <div className='UserPage-container'>
+                <h1 className='greeting'>Welcome, Jake!</h1>
+                <div className='bucketlist-container'>
+                    <h3 className='bucket-header'>Your Bucket List:</h3>
+                    <ul className='bucketlist'>
+                        {this.state.bucketList.map(listObj => {
+                            return (
+                                <li 
+                                    key={listObj.id}
+                                    className='bucketlist-item'
+                                    onClick={() => this.complete(listObj)}
+                                >{listObj.task}</li>
+                            );
+                        })}
+                    </ul>
+                    <form onSubmit={this.addBucketItem}>
+                            <input 
+                                type='text'
+                                name='bucketItem'
+                                placeholder='I really need to...'
+                                value={this.state.bucketItem}
+                                onChange={this.changeHandler}
+                            />
+                            <button onClick={this.addBucketItem}>Add to Bucket</button>
+                    </form>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default UserPage;
